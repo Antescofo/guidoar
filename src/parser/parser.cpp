3bbb87113484e2e88189a21425309c4332fc0261
Arshia Cont <arshia.cont@ircam.fr>
2016-08-11 13:17:11 +0200

    Gets read of symbol clash with guidoLib

--- src/parser/parser.cpp
+++ src/parser/parser.cpp
@@ -30,7 +30,7 @@
 #include <iostream>
 
 #include "gmnreader.h"
-guido::gmnreader * gReader;
+guido::gmnreader * guidoReader;
 
 #define yyleng			guidoarleng
 #define yyin			guidoarin
@@ -57,7 +57,7 @@ namespace guido
 static int parse (FILE *fd) 
 {
 	if (!fd) {
-		gReader->error("Invalid file descriptor", 0 );
+		guidoReader->error("Invalid file descriptor", 0 );
 		return(-1);
 	}
 	yyin = fd;
@@ -77,7 +77,7 @@ static int parse (const char *filename)
 	if( !filename ) return -1; // parse error
 	FILE * fd = fopen(filename,"r");
 	if (fd == NULL){
-		gReader->error("Cannot not open file", 0 );
+		guidoReader->error("Cannot not open file", 0 );
 		return(-1);
 	}
 
@@ -88,7 +88,7 @@ static int parse (const char *filename)
 
 bool readstring (const char * buffer, gmnreader * r) 
 {
-	gReader = r;
+	guidoReader = r;
 
 	if (!*buffer) return false;		// error for empty buffers
 
@@ -108,14 +108,14 @@ bool readstring (const char * buffer, gmnreader * r)
 
 bool readfile (FILE* fd, gmnreader * r) 
 {
-	gReader = r;
+	guidoReader = r;
 	int ret = parse (fd);
  	return ret==0;
 }
 
 bool readfile (const char * file, gmnreader * r) 
 {
-	gReader = r;
+	guidoReader = r;
 	convert_from_unicode (file);
 	int ret = parse (file);
  	return ret==0;
