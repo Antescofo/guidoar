bf290b48f01a5937629cacfba706aaa93302b9a7
Arshia Cont <arshia.cont@ircam.fr>
2016-10-05 14:59:46 +0200

    IOSEPRAC-60 Initiating Clef Change Operation

---
+++ src/operations/clefchangeOperation.cpp
@@ -0,0 +1,148 @@
+//
+//  clefchangeOperation.cpp
+//  guidoar
+//
+//  Created by Arshia Cont on 05/10/16.
+//
+//
+
+#include "clefchangeOperation.hpp"
+
+#ifdef WIN32
+# pragma warning (disable : 4786)
+#endif
+
+#include <iostream>
+#include <math.h>
+
+#include "ARNote.h"
+#include "ARFactory.h"
+#include "AROthers.h"
+#include "ARTag.h"
+#include "clonevisitor.h"
+#include "firstpitchvisitor.h"
+#include "transposeOperation.h"
+#include "tree_browser.h"
+
+using namespace std;
+
+/*****
+ Guido clefs are
+    - "violin"="g"="g2" with octave variants "g+8" and "g-8"
+    - "bass"="f4"="f"   with octave variants
+    -  "alto" = Clef Ut de troisième or alto clef
+    - "tenor" = Clef Ut de 4ême or tenor clef!
+ 
+ See https://fr.wikipedia.org/wiki/Clef_(musique) and https://en.wikipedia.org/wiki/Clef for naming
+ 
+ ****/
+
+namespace guido
+{
+    //________________________________________________________________________
+    // transposeOperation implementation
+    //________________________________________________________________________
+    clefchangeOperation::clefchangeOperation ()
+    {
+        //initialize();
+        staffNumToChange = 0;
+        visitStaff = true;
+    }
+    
+    clefchangeOperation::~clefchangeOperation() {}
+
+    
+    Sguidoelement clefchangeOperation::operator() ( const Sguidoelement& score, std::string clef )
+    {
+        // Todo: Check if clef is valid!
+        newClef = clef;
+        
+        staffNumToChange = 0;   // zero for all staves
+        
+        Sguidoelement transposed;
+        if (score) {
+            guidoScore = score;
+            clonevisitor cv;
+            transposed = cv.clone(score);
+        }
+        if (transposed) {
+            tree_browser<guidoelement> tb(this);
+            tb.browse (*transposed);
+        }
+        return transposed;
+    }
+    
+    Sguidoelement clefchangeOperation::operator() ( const Sguidoelement& score, std::string clef, int staffNumber )
+    {
+        // Todo: Check if clef is valid!
+        newClef = clef;
+        
+        staffNumToChange = staffNumber;
+        
+        Sguidoelement transposed;
+        if (score) {
+            guidoScore = score;
+            clonevisitor cv;
+            transposed = cv.clone(score);
+        }
+        if (transposed) {
+            tree_browser<guidoelement> tb(this);
+            tb.browse (*transposed);
+        }
+        return transposed;
+    }
+
+    
+    //________________________________________________________________________
+    void clefchangeOperation::visitStart ( Sguidotag& elt )
+    {
+        Sguidoattributes attr = elt->attributes();
+        Sguidoattributes::const_iterator iter;
+        
+        int type = elt->getType();
+        if (type == kTClef)
+        {
+            if (visitStaff)
+            {
+                // Just change!
+                attr.at(0)->setValue(newClef, true);
+            }
+        }else if (type==kTStaff)
+        {
+            if (staffNumToChange != 0)
+            {
+                // Check if visiting staff corresponds!
+                if (attr.size())
+                {
+                    int thisStaffNum = atoi(attr.at(1)->getValue().c_str());
+                    if (thisStaffNum != staffNumToChange)
+                    {
+                        visitStaff = false;
+                    }else
+                        visitStaff = true;
+                }
+            }
+        }
+        
+    }
+    
+    
+    bool  clefchangeOperation::isGClef(Sguidotag & elt)
+    {
+        Sguidoattributes attr = elt->attributes();
+        if ((attr.at(0)->getValue()=="g2")||(attr.at(0)->getValue()=="g")||(attr.at(0)->getValue()=="violin"))
+            return true;
+        else
+            return false;
+    }
+    
+    bool  clefchangeOperation::isFClef(Sguidotag & elt)
+    {
+        Sguidoattributes attr = elt->attributes();
+        if ((attr.at(0)->getValue()=="f4")||(attr.at(0)->getValue()=="f")||(attr.at(0)->getValue()=="bass"))
+            return true;
+        else
+            return false;
+    }
+
+}
---
+++ src/operations/clefchangeOperation.hpp
@@ -0,0 +1,92 @@
+//
+//  clefchangeOperation.hpp
+//  guidoar
+//
+//  Created by Arshia Cont on 05/10/16.
+//
+//
+
+#ifndef clefchangeOperation_hpp
+#define clefchangeOperation_hpp
+
+#include <stdio.h>
+#include <map>
+#include <vector>
+
+#include "export.h"
+#include "ARTypes.h"
+#include "guidoelement.h"
+#include "operation.h"
+#include "visitor.h"
+
+
+namespace guido
+{
+    
+    /*!
+     \addtogroup operations
+     @{
+     */
+    
+    /*!
+     \brief A visitor that changes Clef of a given score.
+     */
+    class gar_export clefchangeOperation :
+    public operation,
+    public visitor<SARNote>,
+    public visitor<SARKey>,
+    public visitor<SARVoice>,
+    public visitor<Sguidotag>
+    {
+    public:
+        enum { kUndefinedKey = -99 };
+        
+        typedef int	Chromatic;
+        typedef int	Diatonic;
+        
+        clefchangeOperation();
+        virtual ~clefchangeOperation();
+        
+        /*! Changes a score clef for the entire score!
+         \param score the score to transpose
+         \param clef string destination
+         \return a new score
+         */
+        Sguidoelement operator() ( const Sguidoelement& score, std::string clef );
+        
+        /*! Changes a score clef for the given voice
+         \param score the score to transpose
+         \param clef string destination
+         \param voice number (staff number in Guido)
+         \return a new score
+         */
+        Sguidoelement operator() ( const Sguidoelement& score, std::string clef, int staffNumber );
+        
+        
+    protected:
+        
+        std::string newClef;
+        int staffNumToChange;
+        bool visitStaff;
+        
+        virtual void visitStart ( SARNote& elt );
+        virtual void visitStart ( SARKey& elt );
+        virtual void visitStart ( SARVoice& elt );
+        virtual void visitStart( Sguidotag& elt );
+        
+        
+        bool  isGClef(Sguidotag & elt);
+        bool  isFClef(Sguidotag & elt);
+        
+        Sguidoelement guidoScore;
+        
+    };
+    
+    /*! @} */
+    
+} // namespace MusicXML
+
+
+
+
+#endif /* clefchangeOperation_hpp */
